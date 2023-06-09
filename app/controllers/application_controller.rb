018a1c434a0023aa18357a6060ebd5f3608de8c2
Guilherme Rera <guilhermerera@gmail.com>
2023-06-09 14:37:06 -0300

    changind message as per tutorial request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
 
     def hello
-        render html: "Hello, world!"
+        render html: "hola, mundo!"
     end
 
     def goodbye
-        render html: "Goodbye, world!"
+        render html: "adios, mundo!"
     end
 
 end
