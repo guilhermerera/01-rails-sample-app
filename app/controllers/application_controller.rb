629b9733892a428eb1eb5abbd2ca981379378aef
Guilherme Rera <guilhermerera@gmail.com>
2023-06-09 13:54:27 -0300

    Changed README file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+    def hello
+        render html: "Hello, world!"
+    end
+
+    def goodbye
+        render html: "Goodbye, world!"
+    end
+
 end
